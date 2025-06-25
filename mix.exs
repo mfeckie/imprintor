3fe411399bc066072345efe0a4ed576d4d321af8
Martin Feckie <martin@mfeckie.dev>
2025-06-25 15:35:28 +0800

    Add deps

--- mix.exs
+++ mix.exs
@@ -21,8 +21,10 @@ defmodule Imprintor.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rustler, ">= 0.0.0", optional: true},
+      {:rustler_precompiled, "~> 0.8"},
+      {:jason, "~> 1.4"},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false, warn_if_outdated: true}
     ]
   end
 end
