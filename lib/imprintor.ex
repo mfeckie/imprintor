0ad6cc30e1a146334f20e5c91b79d67c3bb527e8
Martin Feckie <martin@mfeckie.dev>
2025-06-25 21:46:18 +0800

    Tody up

--- lib/imprintor.ex
+++ lib/imprintor.ex
@@ -1,18 +1,2 @@
 defmodule Imprintor do
-  @moduledoc """
-  Documentation for `Imprintor`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Imprintor.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/imprintor_test.exs
+++ test/imprintor_test.exs
@@ -1,8 +1,4 @@
 defmodule ImprintorTest do
   use ExUnit.Case
   doctest Imprintor
-
-  test "greets the world" do
-    assert Imprintor.hello() == :world
-  end
 end
